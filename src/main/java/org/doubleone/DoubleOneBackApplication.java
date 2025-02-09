4cc45c0ef2f0d8f5a3af983fce9fc42e79ca31d0
gkdudans <chrismhy001@gmail.com>
2025-02-10 00:20:15 +0900

    Initial commit

---
+++ src/main/java/org/doubleone/DoubleOneBackApplication.java
@@ -0,0 +1,13 @@
+package org.doubleone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubleOneBackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DoubleOneBackApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/doubleone/DoubleOneBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.doubleone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoubleOneBackApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
