8334b090c3e4422feaa733e69d84ebf40a81ad2e
gkdudans <chrismhy001@gmail.com>
2025-02-12 21:52:19 +0900

    Fix: CohabitationStatus 정의 추가

--- src/main/java/org/doubleone/domain/senior/entity/CohabitationStatus.java
+++ src/main/java/org/doubleone/domain/senior/entity/CohabitationStatus.java
@@ -1,4 +1,10 @@
 package org.doubleone.domain.senior.entity;
 
 public enum CohabitationStatus {
-}
+    ALONE_HOME,          // 독거
+    SPOUSE_HOME,         // 배우자와 동거, 돌봄 시간 중 집에 있음
+    SPOUSE_AWAY,         // 배우자와 동거, 돌봄 시간 중 자리 비움
+    FAMILY_HOME,         // 다른 가족과 동거, 돌봄 시간 중 집에 있음
+    FAMILY_AWAY          // 다른 가족과 동거, 돌봄 시간 중 자리 비움
+  }
+
