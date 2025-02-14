dd9fae622950c86749e913ccdcac95bca5167125
gkdudans <chrismhy001@gmail.com>
2025-02-14 16:10:54 +0900

    Feat: 매칭 생성, 매칭 상태 변경 구현

--- src/main/java/org/doubleone/global/exception/ErrorCode.java
+++ src/main/java/org/doubleone/global/exception/ErrorCode.java
@@ -19,17 +19,21 @@ public enum ErrorCode {
   // Member
   MEMBER_NOT_FOUND(404, "멤버를 찾을 수 없습니다."),
 
-
   // Worker
   WORKER_NOT_FOUND(404, "요양사를 찾을 수 없습니다."),
   WORKER_CONDITION_NOT_FOUND(404, "요양사의 희망근무조건을 찾을 수 없습니다."),
+
+  // Matching
+  MATCHING_NOT_FOUND(404, "매칭을 찾을 수 없습니다."),
 
   // Chat
   CHATROOM_NOT_FOUND(404, "채팅방을 찾을 수 없습니다."),
 
   // Senior
   SENIOR_NOT_FOUND(404, "노인을 찾을 수 없습니다."),
-  SENIOR_CONDITION_NOT_FOUND(404, "노인 근무 조건을 찾을 수 없습니다.");
+  SENIOR_CONDITION_NOT_FOUND(404, "노인 근무 조건을 찾을 수 없습니다."),
+
+  ;
 
   private final int status;
   private final String message;
