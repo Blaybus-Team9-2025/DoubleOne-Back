0b8c4963359a0505e37d9e427b352ede4ea130d0
Hayoung Moon <124586544+gkdudans@users.noreply.github.com>
2025-02-16 16:05:43 +0900

    Revert "[Feat] 요양사 매칭 & 매칭된 요양사 구인정보 조회 API 구현"

--- src/main/java/org/doubleone/domain/condition/controller/ConditionController.java
+++ src/main/java/org/doubleone/domain/condition/controller/ConditionController.java
@@ -1,14 +1,11 @@
 package org.doubleone.domain.condition.controller;
 
-import jakarta.validation.Valid;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.doubleone.domain.condition.dto.ConditionRequestDto;
 import org.doubleone.domain.condition.dto.ConditionResponseDto;
 import org.doubleone.domain.condition.service.ConditionService;
-import org.doubleone.domain.worker.dto.request.WorkerConditionRequestDto;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,16 +20,20 @@ public class ConditionController {
     private final ConditionService conditionService;
 
     @Operation(summary = "근무 조건 등록", description = "관리자가 노인에 대한 근무 조건을 등록")
-    @PostMapping("/{seniorId}")
-    public ResponseEntity<?> createCondition(@PathVariable("seniorId") Long seniorId, @RequestBody ConditionRequestDto requestDto) {
-        conditionService.createCondition(seniorId, requestDto);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    @PostMapping
+    public ResponseEntity<Long> createCondition(@RequestBody ConditionRequestDto requestDto) {
+        Long conditionId = conditionService.createCondition(requestDto);
+        return ResponseEntity.ok(conditionId);
     }
+
     @Operation(summary = "근무 조건 편집", description = "관리자가 노인에 대한 근무 조건을 편집")
-    @PatchMapping("/{seniorId}/workerConditions/{seniorConditionId}")
-    public ResponseEntity<?> updateCondition(@PathVariable("seniorId") Long seniorId, @PathVariable("seniorConditionId") Long seniorConditionId, @RequestBody @Valid ConditionRequestDto requestDto) {
-        conditionService.updateCondition(seniorConditionId, requestDto);
-        return ResponseEntity.status(HttpStatus.OK).build();
+    @PatchMapping("/{conditionId}")
+    public ResponseEntity<Void> updateCondition(
+            @PathVariable Long conditionId,
+            @RequestBody ConditionRequestDto requestDto
+    ) {
+        conditionService.updateCondition(conditionId, requestDto);
+        return ResponseEntity.noContent().build();
     }
 
     @Operation(summary = "근무 조건 삭제", description = "관리자가 노인에 대한 근무 조건을 삭제")
--- src/main/java/org/doubleone/domain/worker/controller/WorkerController.java
+++ src/main/java/org/doubleone/domain/worker/controller/WorkerController.java
@@ -5,17 +5,20 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
 import org.doubleone.domain.worker.dto.response.WorkerDetailResponse;
 import org.doubleone.domain.worker.dto.request.WorkerConditionRequestDto;
+import org.doubleone.domain.worker.service.WorkerService;
 import org.doubleone.domain.workerCondition.service.WorkerConditionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.List;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
@@ -23,7 +26,6 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/workers")
 public class WorkerController {
-
 
     private final WorkerService workerService;
     private final WorkerConditionService workerConditionService;
