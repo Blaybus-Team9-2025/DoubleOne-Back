5fc7190208b7061aa59223e60cc0ebe5312c76b7
gkdudans <chrismhy001@gmail.com>
2025-02-17 19:37:04 +0900

    Feat: 매칭 기록 조회 API 구현

--- src/main/java/org/doubleone/domain/endMatching/repository/EndmatchingRepository.java
+++ src/main/java/org/doubleone/domain/endMatching/repository/EndmatchingRepository.java
@@ -1,8 +1,17 @@
 package org.doubleone.domain.endMatching.repository;
 
+import java.util.List;
 import org.doubleone.domain.endMatching.entity.EndMatching;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface EndmatchingRepository extends JpaRepository<EndMatching, Long> {
 
+  @Query("SELECT e FROM EndMatching e " +
+      "JOIN e.matching m " +
+      "JOIN m.workerCondition c " +
+      "JOIN c.worker w " +
+      "WHERE w.workerId = :workerId")
+  List<EndMatching> findByWorkerId(@Param("workerId") Long workerId);
 }
