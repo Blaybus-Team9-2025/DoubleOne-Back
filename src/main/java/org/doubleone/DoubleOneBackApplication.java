601a9881afbb9417878c7c5d1e119240281493b7
gkdudans <chrismhy001@gmail.com>
2025-02-18 17:49:01 +0900

    Fix: Swagger CORS 오류 해결

--- src/main/java/org/doubleone/DoubleOneBackApplication.java
+++ src/main/java/org/doubleone/DoubleOneBackApplication.java
@@ -1,11 +1,18 @@
 package org.doubleone;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@OpenAPIDefinition(
+    servers = {
+        @Server(url = "https://api.doubleone.p-e.kr", description = "Default Server URL")
+    }
+)
 public class DoubleOneBackApplication {
 
   public static void main(String[] args) {
