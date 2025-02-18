bf63d8dd39a72fe094ca7c6e1adf24b9c571a747
gkdudans <chrismhy001@gmail.com>
2025-02-18 16:41:49 +0900

    Fix: 프론트 로컬 주소 수정

--- src/main/java/org/doubleone/global/config/SecurityConfig.java
+++ src/main/java/org/doubleone/global/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(Arrays.asList(
             "http://localhost:8080",
             "http://localhost:3000",
-            "http://localhost:5173"));
+            "http://localhost:5174"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"));
         configuration.addAllowedHeader("*");
         configuration.setExposedHeaders(Arrays.asList("Set-Cookie", "Location"));
