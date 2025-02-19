3558fdfd1333660c4b95ae38b0afd4808cbb0e90
gkdudans <chrismhy001@gmail.com>
2025-02-20 05:34:01 +0900

    Feat: 채팅 메시지 조회 API 코드 수정

--- src/main/java/org/doubleone/domain/chat/dto/response/ChatMessageListResponseDto.java
+++ src/main/java/org/doubleone/domain/chat/dto/response/ChatMessageListResponseDto.java
@@ -1,9 +1,21 @@
 package org.doubleone.domain.chat.dto.response;
 
 import java.util.List;
+import lombok.Builder;
+import org.doubleone.domain.member.entity.Member;
 
+@Builder
 public record ChatMessageListResponseDto(
+    String name,
+    String profileImg,
     List<ChatMessageResponseDto> chatMessageList
 ) {
 
+  public static ChatMessageListResponseDto from(String name, String profileImg, List<ChatMessageResponseDto> chatMessageList){
+    return ChatMessageListResponseDto.builder()
+        .name(name)
+        .profileImg(profileImg)
+        .chatMessageList(chatMessageList)
+        .build();
+  }
 }
--- src/main/java/org/doubleone/domain/chat/service/ChatMessageService.java
+++ src/main/java/org/doubleone/domain/chat/service/ChatMessageService.java
@@ -8,8 +8,10 @@ import org.doubleone.domain.chat.dto.response.ChatMessageDto;
 import org.doubleone.domain.chat.dto.response.ChatMessageListResponseDto;
 import org.doubleone.domain.chat.dto.response.ChatMessageResponseDto;
 import org.doubleone.domain.chat.entity.ChatMessage;
+import org.doubleone.domain.chat.entity.ChatRoom;
 import org.doubleone.domain.chat.repository.ChatMessageRepository;
 import org.doubleone.domain.chat.repository.ChatRoomRepository;
+import org.doubleone.domain.member.entity.Member;
 import org.doubleone.domain.member.service.MemberService;
 import org.doubleone.global.exception.CustomException;
 import org.doubleone.global.exception.ErrorCode;
@@ -35,13 +37,30 @@ public class ChatMessageService {
 
   @Transactional(readOnly = true)
   public ChatMessageListResponseDto getMessageList(Long chatRoomId) {
-    chatRoomRepository.findById(chatRoomId)
+    ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId)
         .orElseThrow(() -> new CustomException(ErrorCode.CHATROOM_NOT_FOUND));
 
     List<ChatMessage> messages = chatMessageRepository.findAllByChatRoomIdOrderByCreatedAtAsc(chatRoomId);
     List<ChatMessageResponseDto> messageList = messages.stream()
         .map(ChatMessageResponseDto::from)
         .collect(Collectors.toList());
-    return new ChatMessageListResponseDto(messageList);
+
+    // 현재 로그인된 사용자
+    Member currentMember = memberService.getCurrentMember();
+
+    // 상대방 사용자 정보(name, profileImg) 가져오기
+    String name;
+    String profileImg;
+    if (currentMember.equals(chatRoom.getManager().getMember())) {
+      name = chatRoom.getWorker().getName();
+      profileImg = chatRoom.getWorker().getProfileImg();
+    } else {
+      name = chatRoom.getManager().getName();
+      profileImg = chatRoom.getManager().getProfileImg();
+    }
+
+    return new ChatMessageListResponseDto(name, profileImg, messageList);
   }
+
+
 }
